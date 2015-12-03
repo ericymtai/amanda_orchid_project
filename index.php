9f76189305922e630b5422e021d6617ac0ab6772
ericytai <ericym2@gmail.com>
2015-12-03 09:15:19 -0500

    ID;ET
    INIT INDEX.PHP AND FOLDERS

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>AMANDA'S ORCHID HOME</title>
+    <link rel="stylesheet" href="css/newstyle.css" >
+  </head>
+  <body>
+    <header>
+      <div id="logo">
+        <img src="images/logo_outline.svg" alt="logo"/>
+      </div>
+      <nav>
+        <ul>
+          <li>HOME</li>
+          <li>LOGIN</li>
+          <li>LOGOUT</li>
+        </ul>
+      </nav>      
+    </header>
+    <?php
+    // put your code here
+    ?>
+  </body>
+</html>
